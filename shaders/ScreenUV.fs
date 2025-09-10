34d7d630b9b90a72318cac49ed949c21473244e1
Hybrid46 <hybrid_46_2@hotmail.com>
2025-09-10 22:12:24 +0200

    ScreenUV fix -> write black pixels too

--- shaders/ScreenUV.fs
+++ shaders/ScreenUV.fs
@@ -2,13 +2,20 @@
 in vec2 fragTexCoord;
 out vec4 fragColor;
 
-uniform sampler2D _MainTex;
+uniform sampler2D _MainTex; // scene color
 
 void main()
 {
-    float alpha = any(greaterThan(texture(_MainTex, fragTexCoord).rgb, vec3(0.0))) ? 1.0 : 0.0;
-    if (alpha > 0.5)
+    vec4 scene = texture(_MainTex, fragTexCoord);
+    // treat non-black (or alpha) as occluder;
+    float mask = any(greaterThan(scene.rgb, vec3(0.0))) ? 1.0 : 0.0;
+    if (mask > 0.5)
+    {
+        // store the pixel's screen uv in rg
         fragColor = vec4(fragTexCoord, 0.0, 1.0);
+    }
     else
+    {
         fragColor = vec4(0.0);
+    }
 }
