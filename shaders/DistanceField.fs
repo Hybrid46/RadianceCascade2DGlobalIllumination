8ac7cd7ce0595ec5a6147e51a13efd6e9339f653
Hybrid46 <hybrid_46_2@hotmail.com>
2025-09-10 21:06:05 +0200

    Distance Field shader ready
    UV filter?

--- shaders/DistanceField.fs
+++ shaders/DistanceField.fs
@@ -3,14 +3,13 @@ in vec2 fragTexCoord;
 out vec4 fragColor;
 
 uniform sampler2D _MainTex;
-uniform vec2 _Aspect;
 
 void main()
 {
     vec2 uv = texture(_MainTex, fragTexCoord).rg;
     // Calculate distance in screen space
-    vec2 screenPos = fragTexCoord * _Aspect;
-    vec2 storedPos = uv * _Aspect;
+    vec2 screenPos = fragTexCoord;
+    vec2 storedPos = uv;
     float dist = distance(screenPos, storedPos);
     fragColor = vec4(dist, 0.0, 0.0, 1.0);
 }
