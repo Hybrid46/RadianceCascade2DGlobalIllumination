b00321bf9a1fcdf97435c397b816d260d5f1c4f0
Hybrid46 <hybrid_46_2@hotmail.com>
2025-09-12 22:00:25 +0200

    cleanup

--- Program.cs
+++ Program.cs
@@ -46,7 +46,6 @@ class Program
         renderScale = 1.0f;
         rayRange = 2.0f;
 
-        // Unity Original
         //int cascadeWidth = Mathf.CeilToInt((screenWidth * renderScale) / Math.Pow(2, cascadeCount)) * (int)Math.Pow(2, cascadeCount);
         //int cascadeHeight = Mathf.CeilToInt((screenHeight * renderScale) / Math.Pow(2, cascadeCount)) * (int)Math.Pow(2, cascadeCount);
         double powVal = Math.Pow(2, cascadeCount);
@@ -55,45 +54,27 @@ class Program
 
         cascadeResolution = new Vector2Int(cascadeWidth, cascadeHeight);
 
-        // Create render textures
-        /*
-            // Unity Original
-            colorRT, new RenderTextureDescriptor(textureSize.x, textureSize.y, RenderTextureFormat.ARGBFloat, 0), FilterMode.Point);
-            distanceRT, new RenderTextureDescriptor(textureSize.x, textureSize.y, RenderTextureFormat.RHalf, 0), FilterMode.Point);
-
-            jumpFloodRT1, new RenderTextureDescriptor(textureSize.x, textureSize.y, RenderTextureFormat.RGHalf, 0), FilterMode.Point);
-            jumpFloodRT2, new RenderTextureDescriptor(textureSize.x, textureSize.y, RenderTextureFormat.RGHalf, 0), FilterMode.Point);
-
-            giRT1, new RenderTextureDescriptor(cascadeResolution.x, cascadeResolution.y, RenderTextureFormat.ARGBHalf, 0), FilterMode.Bilinear);
-            giRT2, new RenderTextureDescriptor(cascadeResolution.x, cascadeResolution.y, RenderTextureFormat.ARGBHalf, 0), FilterMode.Bilinear); 
-         */
-
         emissiveRT = Raylib.LoadRenderTexture(screenWidth, screenHeight);
-        colorRT.Texture.Format = PixelFormat.UncompressedR32G32B32A32;
-
         colorRT = Raylib.LoadRenderTexture(screenWidth, screenHeight);
-        colorRT.Texture.Format = PixelFormat.UncompressedR32G32B32A32;
-
         distRT = Raylib.LoadRenderTexture(screenWidth, screenHeight);
-        distRT.Texture.Format = PixelFormat.UncompressedR16;
-
         jumpRT1 = Raylib.LoadRenderTexture(screenWidth, screenHeight);
-        jumpRT1.Texture.Format = PixelFormat.UncompressedR16G16B16;
-
         jumpRT2 = Raylib.LoadRenderTexture(screenWidth, screenHeight);
-        jumpRT2.Texture.Format = PixelFormat.UncompressedR16G16B16;
-
         giRT1 = Raylib.LoadRenderTexture(cascadeResolution.x, cascadeResolution.y);
-        giRT1.Texture.Format = PixelFormat.UncompressedR16G16B16A16;
         giRT2 = Raylib.LoadRenderTexture(cascadeResolution.x, cascadeResolution.y);
-        giRT2.Texture.Format = PixelFormat.UncompressedR16G16B16A16;
-
         tempRT = Raylib.LoadRenderTexture(screenWidth, screenHeight);
-        tempRT.Texture.Format = PixelFormat.UncompressedR32G32B32A32;
-        // Initialise all RTs to black
+
+        //emissiveRT.Texture.Format = PixelFormat.UncompressedR32G32B32A32;
+        //colorRT.Texture.Format = PixelFormat.UncompressedR32G32B32A32;
+        //distRT.Texture.Format = PixelFormat.UncompressedR16;
+        //jumpRT1.Texture.Format = PixelFormat.UncompressedR16G16B16;
+        //jumpRT2.Texture.Format = PixelFormat.UncompressedR16G16B16;
+        //giRT1.Texture.Format = PixelFormat.UncompressedR16G16B16A16;
+        //giRT2.Texture.Format = PixelFormat.UncompressedR16G16B16A16;
+        //tempRT.Texture.Format = PixelFormat.UncompressedR32G32B32A32;
+
         ClearAllRTs();
 
-        // Demo geometry (simple walls + moving sprite)
+        // Demo geometry (simple walls)
         List<Rectangle> walls = new List<Rectangle>
         {
             new Rectangle(100, 100, 200, 20),
@@ -212,7 +193,6 @@ class Program
 
         // ---- 1. ScreenUV ----
         //Copying the color texture to another one using Screen UV shader to start the JumpFlood Algorithm
-        //cmd.Blit(colorRT, jumpFloodRT1, screenUVMat); -> Unity original
         Raylib.BeginTextureMode(jumpRT1);
         Raylib.ClearBackground(Color.Black);
         Raylib.BeginShaderMode(screenUV_shader);
@@ -231,27 +211,6 @@ class Program
 
         float stepSize = 1.0f;
 
-        // ---- 2. Jump Flood iterations ----
-        /*
-            for (var n = 0; n < steps; n++)
-            {
-                stepSize *= 0.5f;
-                cmd.SetGlobalFloat("_StepSize", stepSize);
-                //you might find setting this value as global is unecessary but for some reason when using for loops you can't set the value directly to the material with Material.SetFloat
-                //The value don't get passed correctly. Why, I have no idea
-
-                if (jumpFlood1IsFinal)
-                {
-                    cmd.Blit(jumpFloodRT1, jumpFloodRT2, jumpFloodMat);
-                }
-                else
-                {
-                    cmd.Blit(jumpFloodRT2, jumpFloodRT1, jumpFloodMat);
-                }
-
-                jumpFlood1IsFinal = !jumpFlood1IsFinal;
-            }
-         */
         for (int i = 0; i < steps; ++i)
         {
             stepSize *= 0.5f;
@@ -284,18 +243,6 @@ class Program
             jumpFlood1IsFinal = !jumpFlood1IsFinal;
         }
 
-        /*
-            //We check which texture holds the final result and we apply the DistanceField shader to it
-            if (jumpFlood1IsFinal)
-            {
-                cmd.Blit(jumpFloodRT1, distanceRT, distanceFieldMat);
-            }
-            else
-            {
-                cmd.Blit(jumpFloodRT2, distanceRT, distanceFieldMat);
-            }
-         */
-
         // ---- 3. Distance field ----
         RenderTexture2D finalJumpFloodRT = jumpFlood1IsFinal ? jumpRT1 : jumpRT2;
         Raylib.BeginTextureMode(distRT);
@@ -305,39 +252,6 @@ class Program
             new Rectangle(0, 0, finalJumpFloodRT.Texture.Width, -finalJumpFloodRT.Texture.Height),
             Vector2.Zero, Color.White);
         Raylib.EndShaderMode();
-        Raylib.EndTextureMode();
-
-        /*
-            gi1IsFinal = false;//Same as "jumpFlood1IsFinal"
-            for (int i = cascadeCount - 1; i >= 0; i--)
-            {
-                cmd.SetGlobalInt("_CascadeLevel", i);//Again setting it as global cause I can't pass it directly to the material from a for loop
-                                                        //the shader handles the computation of the cascades and the merging at the same time
-                if (gi1IsFinal)
-                {
-                    cmd.Blit(giRT1, giRT2, giMat);
-                }
-                else
-                {
-                    cmd.Blit(giRT2, giRT1, giMat);
-                }
-
-                gi1IsFinal = !gi1IsFinal;
-            }                
-         */
-
-
-        // 3.1 Draw something into giRT1 to start the cascade process
-
-        Raylib.BeginTextureMode(giRT2);
-        Raylib.ClearBackground(Color.Black);
-
-        // moving sprite
-        Vector2 pos = new Vector2(
-            (float)(Raylib.GetTime() * 100f % screenWidth),
-            (float)(Raylib.GetTime() * 75f % screenHeight));
-        Raylib.DrawCircleV(pos, 20f, Color.Lime);
-
         Raylib.EndTextureMode();
 
         // ---- 4. Radiance cascades ----
@@ -363,22 +277,6 @@ class Program
             gi1IsFinal = !gi1IsFinal;
         }
 
-        //----5.Blit GI onto scene----
-        /*
-         cmd.Blit(renderingData.cameraData.renderer.cameraColorTargetHandle, colorRT);
-
-                if (gi1IsFinal)
-                {
-                    blitterMat.SetTexture("_GITex", giRT1);
-                }
-                else
-                {
-                    blitterMat.SetTexture("_GITex", giRT2);
-                }
-
-                cmd.Blit(colorRT, renderingData.cameraData.renderer.cameraColorTargetHandle, blitterMat);//Finaly blending the final result to the camera texture
-         */
-
         //TODO -> Bilinear filtering in shader!
 
         RenderTexture2D finalGI = gi1IsFinal ? giRT1 : giRT2;
@@ -402,19 +300,6 @@ class Program
 
     private static void SetGIShaderValues(Vector2 aspect, int cascadeLevel)
     {
-        /*
-          //Passing values to the GI Shader
-        giMat.SetTexture("_ColorTex", colorRT);
-        giMat.SetTexture("_DistanceTex", distanceRT);
-        giMat.SetFloat("_RayRange", (screen / Mathf.Min(screen.x, screen.y)).magnitude * rayRange);
-        giMat.SetInt("_CascadeCount", cascadeCount);
-        giMat.SetFloat("_SkyRadiance", volume.skyRadiance.value ? 1 : 0);
-        giMat.SetColor("_SkyColor", volume.skyColor.value);
-        giMat.SetColor("_SunColor", volume.sunColor.value);
-        giMat.SetFloat("_SunAngle", volume.sunAngle.value);
-        giMat.SetVector("_CascadeResolution", (Vector2)cascadeResolution);
-        */
-
         // bind textures
         Raylib.SetShaderValueTexture(GI_shader, Raylib.GetShaderLocation(GI_shader, "_ColorTex"), colorRT.Texture);
         Raylib.SetShaderValueTexture(GI_shader, Raylib.GetShaderLocation(GI_shader, "_EmissiveTex"), emissiveRT.Texture);
@@ -432,7 +317,7 @@ class Program
         Raylib.SetShaderValue(GI_shader, Raylib.GetShaderLocation(GI_shader, "_RayRange"), rayRange, ShaderUniformDataType.Float);
 
         // sky params
-        Raylib.SetShaderValue(GI_shader, Raylib.GetShaderLocation(GI_shader, "_SkyRadiance"), 2.0f, ShaderUniformDataType.Float);
+        Raylib.SetShaderValue(GI_shader, Raylib.GetShaderLocation(GI_shader, "_SkyRadiance"), 1.0f, ShaderUniformDataType.Float);
         Raylib.SetShaderValue(GI_shader, Raylib.GetShaderLocation(GI_shader, "_SkyColor"), new Vector3(0.5f, 0.6f, 0.8f), ShaderUniformDataType.Vec3);
         Raylib.SetShaderValue(GI_shader, Raylib.GetShaderLocation(GI_shader, "_SunColor"), new Vector3(1.0f, 0.9f, 0.6f), ShaderUniformDataType.Vec3);
         Raylib.SetShaderValue(GI_shader, Raylib.GetShaderLocation(GI_shader, "_SunAngle"), 0.3f, ShaderUniformDataType.Float);
