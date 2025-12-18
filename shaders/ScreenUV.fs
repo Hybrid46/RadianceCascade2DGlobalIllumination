e2d49ad4498fecec6822b2204edd10878fe1938a
Hybrid46 <hybrid_46_2@hotmail.com>
2025-12-18 12:49:15 +0100

    screenUV.fs -> less branching

--- shaders/ScreenUV.fs
+++ shaders/ScreenUV.fs
@@ -1,21 +1,24 @@
 #version 330 core
+
+precision highp float;
+
 in vec2 fragTexCoord;
 out vec4 fragColor;
 
-uniform sampler2D _MainTex; // scene color
+uniform sampler2D _MainTex;
 
 void main()
 {
-    vec4 scene = texture(_MainTex, fragTexCoord);
-    // treat non-black (or alpha) as occluder;
-    float mask = any(greaterThan(scene.rgb, vec3(0.0))) ? 1.0 : 0.0;
-    if (mask > 0.5)
+    vec4 mainColor = texture(_MainTex, fragTexCoord);
+
+    // treat non-black as occluder;
+    if (any(greaterThan(mainColor.rgb, vec3(0.0))))
     {
         // store the pixel's screen uv in rg
         fragColor = vec4(fragTexCoord, 0.0, 1.0);
     }
     else
     {
-        fragColor = vec4(0.0);
+        fragColor = vec4(0.0, 0.0, 0.0, 1.0);
     }
 }
